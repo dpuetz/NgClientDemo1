ee26574fc6b942861f16e44f2b1ca8caeb679095
dp <dianeofLA@gmail.com>
2018-06-21 16:29:27 -0700

    delete empty folders and move IUser

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IUser } from '../models/iuser';
+import { IUser } from './iuser';
 import { IMessage, Message } from '../shared/imessage';
 import { WebsiteService } from '../websites/website.service';
 import { NgForm } from '@angular/forms';
